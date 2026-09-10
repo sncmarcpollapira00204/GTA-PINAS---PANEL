b9281b293300f81cb03b9ae0639f1829c56a18e0
Lupin <sncmarcpollapira00204@gmail.com>
2026-09-10 12:51:18 +0800

    Add donation embed load and update routes

--- routes/donation.routes.js
+++ routes/donation.routes.js
@@ -6,6 +6,8 @@ const donationEmbedController = require('../controllers/donationEmbed.controller
 const router = express.Router();
 
 router.get('/channels', donationEmbedController.getDonationChannels);
+router.get('/message', donationEmbedController.getMessage);
 router.post('/embed', donationEmbedController.sendEmbed);
+router.patch('/message', donationEmbedController.updateMessage);
 
 module.exports = router;
