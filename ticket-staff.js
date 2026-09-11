a1327cec94d42b9eca114c86fcffe3c732118213
Lupin <sncmarcpollapira00204@gmail.com>
2026-09-11 13:10:58 +0800

    Restore required ticket-staff.js dependency

---
+++ ticket-staff.js
@@ -0,0 +1,34 @@
+module.exports = {
+    ticketAssignAllowedRoles: [
+        '1501546329682346064', // Owner
+        '1501546391082766416', // Owner (Toyo)
+        '1501546425492836393', // Executive Council
+        '1501546455565865000', // High Council
+        '1501546479419134012'  // Sophomore Council
+    ],
+
+    ticketCategories: {
+        report: '1502224121684037742',
+        banAppeal: '1502224071272960010',
+        partnership: '1502224937392541727',
+        povCheck: '1502224356166602852',
+        boostClaim: '1502224518209474631'
+    },
+
+    ticketAssignOptions: [
+        { label: 'YORME', userId: '852940366230323250', categoryId: '1505162819065221130', description: 'Faction Moderator' },
+        { label: 'ASTRIA', userId: '862348507863449631', categoryId: '1505162819065221130', description: 'Admin' },
+        { label: 'BALONG', userId: '782877398860562472', categoryId: '1505162742951186542', description: 'Mapping Team' },
+        { label: 'BROMINE', userId: '474487966539055104', categoryId: '1505162742951186542', description: 'Gang Moderator' },
+        { label: 'DIZEUCE', userId: '311658010906984448', categoryId: '1505162470224953394', description: 'Gang Moderator' },
+        { label: 'GUILLOTINE', userId: '992789429343223828', categoryId: '1505162470224953394', description: 'Faction Moderator' },
+        { label: 'GYDEON', userId: '692015347192299560', categoryId: '1505162371499298877', description: 'Mapping Team' },
+        { label: 'QUINTILIAN', userId: '217260561224695808', categoryId: '1505162371499298877', description: 'Admin' },
+        { label: 'TRISHA', userId: '679270913719533569', categoryId: '1505162218583625818', description: 'Faction Moderator' },
+        { label: 'XINDO', userId: '634627698584715290', categoryId: '1505162218583625818', description: 'Gang Moderator' },
+        { label: 'LUPIN', userId: '735045975378362401', categoryId: '1505162218583625818', description: 'Discord Moderator' },
+        { label: 'CIPHER', userId: '386557507327361025', categoryId: null, description: 'Main Developer' },
+        { label: 'AEZAKMI', userId: '404293317640323073', categoryId: null, description: 'Developer' },
+        { label: 'ZOEY', userId: '706845659533148171', categoryId: null, description: 'Owner' }
+    ]
+};
